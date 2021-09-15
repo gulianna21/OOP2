8d60e71eb0e6408ea22162d66a423445b428bc4d
Bindi798 <odyvan333>
2021-09-15 22:32:14 +0300

    add plate

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,17 +1,24 @@
 public class Main {
     public static void main(String[] args) {
-        Cat cat1 = new Cat();
-        Cat cat2 = new Cat();
-        Cat cat3 = new Cat();
-        Cat cat4 = new Cat();
-        Dog dog1 = new Dog();
-        dog1.run(200);
-        dog1.swim(300);
-        cat1.swim(150);
-        cat1.run(600);
-        cat2.swim(300);
-        cat2.run(190);
-        System.out.println("All animals created: " + cat1.getCount() +
-                " count, " + cat1.getCountCats() + " cats and " + dog1.getCountDogs() + " dogs");
+        Cat[] allCats = new Cat[4];
+        allCats[0] = new Cat("Аська", 50);
+        allCats[1] = new Cat("Куркума", 30);
+        allCats[2] = new Cat("Авокадушко", 20);
+        allCats[3] = new Cat("Мася", 80);
+
+        Plate newPlate = new Plate(100);
+        newPlate.info();
+
+        for (Cat allCat : allCats) {
+            if (!allCat.getFullness() && allCat.getAppetite() <= newPlate.getFood()) {
+                allCat.eat(newPlate);
+                System.out.println("Котэ " + allCat.getName() + " покушал!");
+            } else {
+                System.out.println("Котэ " + allCat.getName() + " не поел!");
+            }
+        }
+        newPlate.info();
+
+        newPlate.increaseFood(500);
     }
 }
